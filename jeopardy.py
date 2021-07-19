f1e1d772a7d0d73eb05ec6b86b7a11848ac5ebe9
Elissa Levy <elissadunnlevy@gmail.com>
2021-07-19 02:21:08 +0000

    lets the user choose the category

--- jeopardy.py
+++ jeopardy.py
@@ -1,5 +1,42 @@
 import requests
+import random
 
-response = requests.get('http://jservice.io/api/clues?category=139').json()
+categoriesRequested = 100
+categories = requests.get(f'http://jservice.io/api/categories?count={categoriesRequested}').json()
 
-print(response)
+categoriesOffered = 5
+categories = random.choices(categories, k=categoriesOffered)
+
+print("Your available categories are:")
+for category in categories:
+    print(category["title"])
+
+choice = input("Which of these categories do you choose? ")
+choiceID = 0
+while choiceID == 0:
+    for category in categories:
+        if category["title"] == choice:
+            choiceID = category["id"]
+    if choiceID == 0:
+        choice = input("Please try again.  Which category do you choose? ")
+
+response = requests.get(f'http://jservice.io/api/clues?category={choiceID}').json()
+
+print("Your first question is...")
+userAns = input(response[0]["question"] + "... ")
+if userAns == response[0]["answer"]:
+    print("You got it right!")
+else:
+    print("Sorry, that was incorrect.")
+
+# for i in response:
+#     print(i["category"]["title"])
+# print(len(response))
+# print(response[0]["category"]["title"])
+
+# for i in response:
+#     userAns = input(i["question"] + " ")
+#     if userAns == i["answer"]:
+#         print("Hooray!")
+#     else:
+#         print("WRONG")
